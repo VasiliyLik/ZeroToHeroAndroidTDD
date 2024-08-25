2b5f467edf73171ca400d7027e85f26c4b861d3f
VasiliyLik <vasya_li@mail.ru>
2024-08-25 13:39:58 +0300

    task/task012-increment-max solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,44 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var uiState: UiState = UiState.Base("0")
+    private val count = Count.Base(2, 4)
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.incrementButton.setOnClickListener {
+            uiState = count.increment(binding.countTextView.text.toString())
+            uiState.apply(binding.countTextView, binding.incrementButton)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(KEY, uiState)
+
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        uiState = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
+            savedInstanceState.getSerializable(KEY, UiState::class.java) as UiState
+        else
+            savedInstanceState.getSerializable(KEY) as UiState
+        uiState.apply(binding.countTextView, binding.incrementButton)
+    }
+
+    companion object {
+        private const val KEY = "uiStateKey"
     }
 }
