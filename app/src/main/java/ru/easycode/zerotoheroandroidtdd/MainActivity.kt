0f1266ccd07c9457cbd61c7c41a6684128395ae6
VasiliyLik <vasya_li@mail.ru>
2024-08-29 22:05:21 +0300

    task/task016-progress-mvvm-recreate solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = (application as App).viewModel
+
+        binding.actionButton.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) { uiState ->
+            uiState.apply(binding.actionButton, binding.progressBar, binding.titleTextView)
+        }
     }
 }
