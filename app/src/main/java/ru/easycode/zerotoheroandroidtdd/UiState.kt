bd19f6e56fccca2c114d60bb15d710e188aeb72a
VasiliyLik <vasya_li@mail.ru>
2024-09-01 20:47:50 +0300

    task/task018-clouddatasource solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,32 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import java.io.Serializable
+
+interface UiState : Serializable {
+
+    fun apply(textView: TextView, progressBar: ProgressBar, button: Button)
+
+    object ShowProgress : UiState {
+        private fun readResolve(): Any = ShowProgress
+
+        override fun apply(textView: TextView, progressBar: ProgressBar, button: Button) {
+            textView.visibility = View.INVISIBLE
+            progressBar.visibility = View.VISIBLE
+            button.isEnabled = false
+        }
+    }
+
+    data class ShowData(private val text: String) : UiState {
+
+        override fun apply(textView: TextView, progressBar: ProgressBar, button: Button) {
+            textView.visibility = View.VISIBLE
+            textView.text = text
+            progressBar.visibility = View.INVISIBLE
+            button.isEnabled = true
+        }
+    }
+}
