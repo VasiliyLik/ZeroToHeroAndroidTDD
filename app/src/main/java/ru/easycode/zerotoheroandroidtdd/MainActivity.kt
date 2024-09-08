c0f277d58477537e5a325327cb45d39c967f501d
VasiliyLik <vasya_li@mail.ru>
2024-09-08 13:40:48 +0300

    task/task022-list solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,51 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.children
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            val inputText = binding.inputEditText.text
+            addTextView(inputText.toString())
+            binding.inputEditText.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val textList =
+            binding.contentLayout.children.map { (it as TextView).text.toString() }.toList()
+        outState.putStringArrayList(KEY, ArrayList(textList))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val list = savedInstanceState.getStringArrayList(KEY) ?: ArrayList()
+        list.forEach { inputText ->
+            addTextView(inputText)
+        }
+    }
+
+    private fun addTextView(inputText: CharSequence) {
+        val textView = TextView(this).apply {
+            text = inputText
+        }
+        binding.contentLayout.addView(textView)
+    }
+
+    companion object {
+        private const val KEY = "list"
     }
 }
